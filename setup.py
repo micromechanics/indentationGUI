b236d08918730da9d95e39b266758812de0707fe
Weiguang YANG <weiguang.yang@yahoo.com>
2023-04-14 10:00:21 +0200

    setting setup for pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,7 @@ from setuptools import setup
 import commit
 
 if __name__ == '__main__':
-    setup(name='micromechanics-indentationGUI',
+    setup(
+          name='micromechanics-indentationGUI',
           version=commit.get_version()[1:],
-          entry_points={
-                          'console_scripts': 
-                                            [
-                                              'micromechanics-indentationGUI=micromechanics-indentationGUI',
-                                            ]
-                        }
           )
