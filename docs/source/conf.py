947593554038c1c3886f2f4066b0467121d7b32e
Weiguang YANG <weiguang.yang@yahoo.com>
2023-09-03 20:02:24 +0200

    'v0.1.12'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ copyright = u'2022-{}, Micromechanics team'.format(datetime.datetime.now().year)
 author = u'Micromechanics team'
 
 # The full version, including alpha/beta/rc tags
-version = "0.1.11"
+version = "0.1.12"
 release = version
 
 # -- General configuration ---------------------------------------------------
